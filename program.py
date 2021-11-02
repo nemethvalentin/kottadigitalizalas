be9a9abf78890e24a541079bac7cce93030a20cf
Németh Valentin <valentin001998@gmail.com>
2021-11-02 17:36:41 +0100

    Vonalkeresés finomítása és további tesztelések

--- program.py
+++ program.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('kepek/tesztkepek/test2.jpg')
+img = cv2.imread('kepek/tesztkepek/test1.jpg')
+
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 thresh= cv2.threshold(gray,100,255,cv2.THRESH_BINARY)[1]
 thresh = 255-thresh
 thresh = cv2.dilate(thresh, cv2.getStructuringElement(cv2.MORPH_RECT, (3,3)))
-minLineLength = img.shape[1]/4*3
-maxLineGap = 10
+minLineLength = img.shape[1]*0.75
+maxLineGap = 25
 lines = cv2.HoughLinesP(thresh,1,np.pi/2,1000,minLineLength,maxLineGap)
 goodLines = []
 for line in lines:
@@ -24,13 +25,16 @@ i=0
 counter = 0
 averageGap = 0
 while i<len(goodLines):
-    tempLines.append(goodLines[i])
-    beforeY = goodLines[i][1]
+    tempLine=goodLines[i]
+    beforeY = tempLine[1]
     j=i+1
     while j<len(goodLines) and goodLines[j][1]-beforeY <= 5:
+        if abs(goodLines[j][2]-goodLines[j][0]) > abs(tempLine[2]-tempLine[0]) :
+            tempLine = goodLines[j]
         j += 1
+
     
-   
+    tempLines.append(tempLine)
     i=j+1
     if len(tempLines) == 5:
         goodLines2.append(tempLines)
@@ -42,15 +46,14 @@ averageGap = averageGap/len(goodLines2)
 
 for lines in goodLines2:
     for line in lines:
-        cv2.line(img,(line[0],line[1]), (line[2],line[3]), (255,255,255), round(averageGap/5))
-        cv2.line(thresh,(line[0],line[1]), (line[2],line[3]), (0,0,0), round(averageGap/5))
+        cv2.line(img,(line[0],line[1]), (line[2],line[3]), (255,255,255), round(averageGap/4))
+        cv2.line(thresh,(line[0],line[1]), (line[2],line[3]), (0,0,0), round(averageGap/4))
 
 for lines in goodLines2:
     print(lines)
 print(len(goodLines2))
 
 thresh = cv2.erode(thresh,cv2.getStructuringElement(cv2.MORPH_RECT,(3,3)),iterations=3)
-
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 for cnt in contours:
@@ -61,6 +64,7 @@ for cnt in contours:
 
     cv2.drawContours(img, contours, -1, (255,0,255), thickness=2)
 
-cv2.imwrite('linetest.jpg',img)
-cv2.imwrite('linethresh.jpg',thresh)
+
+cv2.imwrite('linesandcontours1.jpg',img)
+cv2.imwrite('linesandcontoursthresh1.jpg',thresh)
 cv2.waitKey(0)
