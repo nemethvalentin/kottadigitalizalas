43e6d1d6b3c5facc5252404a484765866fe03d2a
nemethvalentin <valentin001998@gmail.com>
2021-11-23 22:15:32 +0100

    Modell működésének tesztelése

---
+++ testCNN.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow import keras
+import cv2
+import numpy as np
+
+test_img = cv2.imread('ujtemplatek/template55.png')
+test_gray = cv2.cvtColor(test_img,cv2.COLOR_BGR2GRAY)
+test_thresh = cv2.threshold(test_gray,100,255,cv2.THRESH_BINARY)[1]
+
+model = keras.models.load_model('symbol_classification.h5')
+test_img=cv2.resize(test_img, (32, 32), interpolation=cv2.INTER_CUBIC)
+test_img=test_img.reshape(-1, 32, 32, 3)
+prediction = model.predict(test_img)
+print(np.argmax(prediction[0]))
